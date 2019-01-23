e2b764546fefc32cc75b40be3cbed10a003be862
Flavio Torres <ftorres@walmartlabs.com>
2019-01-22 17:48:35 -0800

    supermicro_power_on_off_redfish.sh

---
+++ supermicro_power_on_off_redfish.sh
@@ -0,0 +1,25 @@
+SERVER="my-server"
+USER="ADMIN"
+PASS="ADMIN"
+
+# ON
+curl -si -u $USER:$PASS -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"Action": "Reset", "ResetType": "On"}' https://$SERVER/redfish/v1/Systems/1/Actions/ComputerSystem.Reset
+
+#OFF
+curl -si -u $USER:$PASS -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"Action": "Reset", "ResetType": "ForceOff"}' https://$SERVER/redfish/v1/Systems/1/Actions/ComputerSystem.Reset
+
+# Other methods available:
+curl -si -u $USER:$PASS -k -XGET https://$SERVER/redfish/v1/Systems/1/
+
+    "Actions": {
+        "#ComputerSystem.Reset": {
+            "target": "/redfish/v1/Systems/1/Actions/ComputerSystem.Reset",
+            "ResetType@Redfish.AllowableValues": [
+                "On",
+                "ForceOff",
+                "GracefulShutdown",
+                "GracefulRestart",
+                "ForceRestart",
+                "Nmi",
+                "ForceOn"
+            ]
